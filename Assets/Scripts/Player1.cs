9fe919b6190591823d997126c5fff1500a003d1b
Christpher Aucoin <criscross03@gmail.com>
2018-02-05 16:27:02 -0600

    Started working on player controller

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour {
+
+	public float speed;
+	private float upLimit=4.4f;
+	private float downLimit = -4.4f;
+
+	Transform trans;
+
+	// Use this for initialization
+	void Start () {
+		trans = GetComponent<Transform> ();
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("w") && trans.position.y < upLimit) {
+			trans.Translate (0.0f, speed, 0.0f);
+		} else if (Input.GetKey ("s") && trans.position.y > downLimit) {
+			trans.Translate (0.0f, -speed, 0.0f);
+		}
+	}
+}
---
+++ Assets/Scripts/Pong.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pong : MonoBehaviour {
+
+	float xVec;
+	float yVec;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
